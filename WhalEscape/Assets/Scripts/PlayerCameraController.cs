eaf42e0d2880c10ccd17d757d2d00ea3c9162b2e
Owen McGill <mcgillowen@gmail.com>
2018-03-19 16:19:27 +0100

    PlayerCameraController

---
+++ WhalEscape/Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+
+	public GameObject Jonah;
+
+	private Vector3 _cameraOffset;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_cameraOffset = transform.position - Jonah.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Jonah.transform.position + _cameraOffset;
+	}
+}
